0dfbbf6d85a4a8457759eb8d99d354639ce7aff2
Anastasia Magopoulou <105965911+anastasiamag@users.noreply.github.com>
2023-03-26 19:17:44 +0300

    changed details in homescreen

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,25 +7,37 @@ import {
   FlatList,
   TouchableOpacity,
 } from "react-native";
+import { withNavigation } from "react-navigation";
+import HeartPress from "../components/HeartPress";
 
 const Details = ({ places, navigation }) => {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, backgroundColor: "#E2E8E9" }}>
       <View style={{ flex: 1 }}>
         <FlatList
           showsVerticalScrollIndicator={false}
           data={places}
           renderItem={({ item }) => {
             return (
-              <View>
+              <View style={styles.container}>
                 <TouchableOpacity
-                  onPress={() => navigation.navigate("Details")}
+                  onPress={() =>
+                    navigation.navigate("Details", { places: item })
+                  }
                 >
                   <View style={styles.list}>
-                    <Image style={styles.image} source={item.image} />
-                  </View>
-                  <View>
-                    <Text style={styles.places}>{item.name}</Text>
+                    <View>
+                      <Image style={styles.image} source={item.image} />
+                    </View>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <Text style={styles.places}>{item.name}</Text>
+                      <HeartPress />
+                    </View>
                   </View>
                 </TouchableOpacity>
               </View>
@@ -40,24 +52,62 @@ const Details = ({ places, navigation }) => {
 const styles = StyleSheet.create({
   list: {
     flex: 1,
-    flexDirection: "row",
     paddingVertical: 4,
-    justifyContent: "center",
-    backgroundColor: "#C0C6CD",
-    borderRadius: 25,
+    justifyContent: "space-evenly",
+    backgroundColor: "#F3F6F6",
+    borderRadius: 20,
     marginHorizontal: 8,
-    height: 260,
+    marginVertical: 8,
   },
   places: {
     fontSize: 22,
     marginLeft: 8,
+    marginTop: 8,
   },
   image: {
-    alignSelf: "center",
     height: "100%",
-    width: "98%",
+    width: "100%",
     borderRadius: 20,
+    height: 260,
+  },
+  container: {
+    borderWidth: 1.75,
+    borderColor: "#ccc",
+    borderRadius: 20,
+    marginBottom: 10,
+    backgroundColor: "#F3F6F6",
+    marginHorizontal: 8,
+    marginTop: 8,
   },
 });
 
-export default Details;
+export default withNavigation(Details);
+
+// container: {
+//   flex: 1,
+//   flexDirection: "column",
+//   alignItems: "center",
+//   justifyContent: "space-evenly",
+//   paddingTop: 40,
+//   paddingBottom: 50,
+
+//   backgroundColor: "#C0C6CD",
+//   borderRadius: 25,
+//   marginHorizontal: 5,
+//   marginVertical: 12,
+// },
+// list: {
+//   height: 150,
+//   width: 200,
+// },
+// places: {
+//   fontSize: 18,
+//   // marginTop: 40,
+//   marginLeft: 8,
+//   justifyContent: "center",
+// },
+// image: {
+//   height: "100%",
+//   width: "98%",
+//   borderRadius: 20,
+// },
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ import {
   Image,
   TouchableOpacity,
 } from "react-native";
-import HeartPress from "../components/HeartPress";
+
 import Details from "../components/Details";
 
 const places = [
@@ -16,21 +16,36 @@ const places = [
     image: {
       uri: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/7f/b5/b1/museum.jpg?w=1000&h=-1&s=1",
     },
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   },
   {
     name: "Αρχαιολογικό Μουσείο Κοζάνης",
     image: {
       uri: "https://upload.wikimedia.org/wikipedia/commons/7/73/Macedonian_Museums-2-Arx_Kozanis-8.jpg",
     },
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   },
   {
     name: "Αρχοντικό Γρ. Βούρκα",
     image: {
       uri: "https://lh5.googleusercontent.com/p/AF1QipOPEuKpSAysd7rnm6_fu8bXeFjazqMcF1HYhSg5=w243-h174-n-k-no-nu",
     },
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   },
-  { name: "Place 4", image: require("../../assets/beach.jpg") },
-  { name: "Place 5", image: require("../../assets/beach.jpg") },
+  {
+    name: "Πλατεία Νίκης",
+    image: {
+      uri: "https://underwriterpress.com.cy/wp-content/uploads/2016/05/pegasus_LARGE_t_242261_54431829-700x469.jpg",
+    },
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    name: "Κοβεντάρειος Δημοτική Βιβλιοθήκη Κοζάνης",
+    image: {
+      uri: "https://lh3.googleusercontent.com/p/AF1QipM2vC1R9EMzmFJ_20-T_74-BxYF6r1XaWGx5pRJ=s680-w680-h510",
+    },
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
 ];
 
 const HomeScreen = ({ navigation }) => {
@@ -38,26 +53,25 @@ const HomeScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  list: {
-    flex: 1,
-    flexDirection: "row",
-    paddingVertical: 4,
-    justifyContent: "center",
-    backgroundColor: "#C0C6CD",
-    borderRadius: 25,
-    marginHorizontal: 8,
-    height: 260,
-  },
-  places: {
-    fontSize: 22,
-    marginLeft: 8,
-  },
-  image: {
-    alignSelf: "center",
-    height: "100%",
-    width: "98%",
-    borderRadius: 20,
-  },
+  // list: {
+  //   flex: 1,
+  //   flexDirection: "row",
+  //   padding: 10,
+  //   justifyContent: "center",
+  //   backgroundColor: "#C0C6CD",
+  //   borderRadius: 25,
+  //   marginHorizontal: 8,
+  //   height: 260,
+  // },
+  // places: {
+  //   fontSize: 22,
+  //   marginLeft: 8,
+  // },
+  // image: {
+  //   height: "100%",
+  //   width: "98%",
+  //   borderRadius: 20,
+  // },
 });
 
 export default HomeScreen;
