ad9a059dccff89503937362c1e2b12dbb1a5b009
Anastasia Magopoulou <93059645+katrhss@users.noreply.github.com>
2023-03-29 00:32:15 +0300

    Update tracker.js

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://1e94-2a02-586-6a3a-ffc0-3192-1e4f-46f7-90b1.eu.ngrok.io",
+  baseURL: "https://smart-kozani.herokuapp.com",
 });
